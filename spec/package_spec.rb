bb80bc1e10ad395bbd3fbf26c214f5d43592369a
Leigh Dodds <leigh@ldodds.com>
2013-12-04 17:42:29 +0000

    Temporarily removed unimplemented fixtures

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -183,17 +183,17 @@ describe DataPackage::Package do
             
     end
     
-    context "when validating with the simpledataformat profile" do
-        it "should ensure that resources have a schema"
-        it "should validate the schema of a resource"
-        it "should validate the CSVDDF dialect of a resource"
-        
-        it "should ensure that every resource is a CSV file"
-        
-        it "should ensure that every CSV file has a header"
-        it "should detect fields missing from CSV file"
-        it "should detect fields missing from schema"
-        
-        #it "should check encoding of CSV files is UTF-8"
-    end 
+#    context "when validating with the simpledataformat profile" do
+#        it "should ensure that resources have a schema"
+#        it "should validate the schema of a resource"
+#        it "should validate the CSVDDF dialect of a resource"
+#        
+#        it "should ensure that every resource is a CSV file"
+#        
+#        it "should ensure that every CSV file has a header"
+#        it "should detect fields missing from CSV file"
+#        it "should detect fields missing from schema"
+#        
+#        #it "should check encoding of CSV files is UTF-8"
+#    end 
 end
