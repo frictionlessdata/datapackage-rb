6a92b71961b8198fbd78883ea61c457d53089c35
Leigh Dodds <leigh@ldodds.com>
2013-12-05 13:22:28 +0000

    Separated out tests for individual validators into separate spec files

---
+++ spec/simpledataformatvalidator_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe DataPackage::SimpleDataFormatValidator do
+    
+    #    context "when validating with the simpledataformat profile" do
+    #        it "should ensure that resources have a schema"
+    #        it "should validate the schema of a resource"
+    #        it "should validate the CSVDDF dialect of a resource"
+    #        
+    #        it "should ensure that every resource is a CSV file"
+    #        
+    #        it "should ensure that every CSV file has a header"
+    #        it "should detect fields missing from CSV file"
+    #        it "should detect fields missing from schema"
+    #        
+    #        #it "should check encoding of CSV files is UTF-8"
+    #    end 
+
+end
