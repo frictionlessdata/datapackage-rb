1e17334fa906ecb88b951cbbcfe43222e01dc9ec
pikesley <github@orgraphone.org>
2016-09-05 11:10:01 +0100

    Atom dinged lots of whitespace

--- spec/datapackagevalidator_spec.rb
+++ spec/datapackagevalidator_spec.rb
@@ -1,83 +1,83 @@
 describe DataPackage::DataPackageValidator do
-    
+
     before(:each) do
         @validator = DataPackage::DataPackageValidator.new()
     end
-    
+
     it "should distinguish between errors and warnings" do
         package = DataPackage::Package.new(test_package_filename)
         messages = @validator.validate( package )
         expect( messages[:errors] ).to eql([])
         expect( messages[:warnings] ).to eql([])
-        
-        package = DataPackage::Package.new( { 
-            "name" => "testing", 
-            "licenses"=>[{"id"=>"", "url"=>""}], 
-            "datapackage_version"=>"" 
+
+        package = DataPackage::Package.new( {
+            "name" => "testing",
+            "licenses"=>[{"id"=>"", "url"=>""}],
+            "datapackage_version"=>""
         }, { :base => Dir.tmpdir } )
         messages = @validator.validate( package )
         #missing resources
         expect( messages[:errors].length ).to eql(1)
         msg = messages[:errors][0]
         expect( msg[:type] ).to eql(:metadata)
-            
+
         #missing README
         #Note: slightly fragile as this will fail if there's a README.md in /tmp
         expect( messages[:warnings] ).to_not be_empty
         msg = messages[:warnings][0]
         expect( msg[:type] ).to eql(:integrity)
-                                    
-    end  
-    
+
+    end
+
     it "should provide warnings about missing useful keys" do
-        package = DataPackage::Package.new( { 
+        package = DataPackage::Package.new( {
             "name" => "testing",
             "resources" => [ { "path" => "data.csv" }]
         } )
         messages = @validator.validate( package )
-        expect( messages[:warnings] ).to_not be_empty                                     
+        expect( messages[:warnings] ).to_not be_empty
     end
-    
+
     it "should treat warnings as errors in strict mode" do
-        package = DataPackage::Package.new( { 
-            "name" => "testing" 
+        package = DataPackage::Package.new( {
+            "name" => "testing"
         } )
-        messages = @validator.valid?( package )            
+        messages = @validator.valid?( package )
     end
-    
+
     it "should warn if README.md is missing" do
         package = DataPackage::Package.new(test_package_filename)
         messages = @validator.validate( package )
         expect( messages[:warnings] ).to be_empty
 
-        FakeWeb.register_uri(:get, "http://example.com/datapackage.json", 
+        FakeWeb.register_uri(:get, "http://example.com/datapackage.json",
             :body => File.read( test_package_filename ) )
-        FakeWeb.register_uri(:head, "http://example.com/README.md", 
+        FakeWeb.register_uri(:head, "http://example.com/README.md",
             :body => "", :status=>["404", "Not Found"] )
-                                                                   
+
         package = DataPackage::Package.new( "http://example.com/" )
         messages = @validator.validate( package )
-        expect( messages[:warnings] ).to_not be_empty                              
+        expect( messages[:warnings] ).to_not be_empty
     end
-    
+
     it "should check that all files are accessible" do
         package = DataPackage::Package.new(test_package_filename)
         messages = @validator.validate( package )
         expect( messages[:errors] ).to be_empty
-            
+
         data = JSON.parse( File.read( test_package_filename ) )
         #refer to missing resource
         data["resources"][0]["path"] = "oh-dear-its-gone.csv"
         #point test package at new base dir
         package = DataPackage::Package.new(data, { :base => File.join( File.dirname(__FILE__), "test-pkg") })
         messages = @validator.validate( package )
-        expect( messages[:errors] ).to_not be_empty                                            
+        expect( messages[:errors] ).to_not be_empty
     end
-    
+
     it "should check resource urls not just resource paths" do
-        FakeWeb.register_uri(:head, "http://example.com/resource.csv", 
+        FakeWeb.register_uri(:head, "http://example.com/resource.csv",
             :body => "data,here" )
-        
+
         data = JSON.parse( File.read( test_package_filename ) )
         #refer to missing resource
         data["resources"][0].delete("path")
@@ -85,7 +85,7 @@ describe DataPackage::DataPackageValidator do
         #point test package at new base dir
         package = DataPackage::Package.new(data, { :base => File.join( File.dirname(__FILE__), "test-pkg") })
         messages = @validator.validate( package )
-        expect( messages[:errors] ).to be_empty                                                        
-    end    
-        
+        expect( messages[:errors] ).to be_empty
+    end
+
 end
--- spec/simpledataformatvalidator_spec.rb
+++ spec/simpledataformatvalidator_spec.rb
@@ -1,54 +1,54 @@
 describe DataPackage::SimpleDataFormatValidator do
-    
+
     before(:each) do
         @validator = DataPackage::SimpleDataFormatValidator.new()
     end
-        
+
     it "should ensure that resources have a schema" do
         package = DataPackage::Package.new(test_package_filename)
         expect( @validator.valid?( package ) ).to be(true)
-        
-        package.resources[0].delete("schema")            
-        expect( @validator.valid?( package ) ).to be(false)        
+
+        package.resources[0].delete("schema")
+        expect( @validator.valid?( package ) ).to be(false)
     end
-    
+
     it "should validate the schema of a resource" do
         package = DataPackage::Package.new(test_package_filename)
-        package.resources[0]["schema"]["fields"][0].delete("name")            
-        expect( @validator.valid?( package ) ).to be(false)                        
+        package.resources[0]["schema"]["fields"][0].delete("name")
+        expect( @validator.valid?( package ) ).to be(false)
     end
-    
-    
+
+
     it "should validate the CSVDDF dialect of a resource" do
         package = DataPackage::Package.new(test_package_filename)
-        package.resources[0]["dialect"].delete("delimiter")            
-        expect( @validator.valid?( package ) ).to be(false)                                
+        package.resources[0]["dialect"].delete("delimiter")
+        expect( @validator.valid?( package ) ).to be(false)
     end
-            
+
     it "should ensure that every resource is a CSV file" do
         package = DataPackage::Package.new(test_package_filename)
         package.resources[0].delete("format")
-        package.resources[0].delete("mediatype")           
-        expect( @validator.valid?( package ) ).to be(true)                                        
+        package.resources[0].delete("mediatype")
+        expect( @validator.valid?( package ) ).to be(true)
     end
 
     it "should correctly identify CSV files" do
-        package = DataPackage::Package.new(test_package_filename)           
-        expect( @validator.csv?( package.resources[0], package ) ).to be(true)                                        
-        package.resources[0].delete("mediatype")                   
-        expect( @validator.csv?( package.resources[0], package ) ).to be(true)                                        
-        package.resources[0].delete("format") 
+        package = DataPackage::Package.new(test_package_filename)
+        expect( @validator.csv?( package.resources[0], package ) ).to be(true)
+        package.resources[0].delete("mediatype")
+        expect( @validator.csv?( package.resources[0], package ) ).to be(true)
+        package.resources[0].delete("format")
         expect( @validator.csv?( package.resources[0], package ) ).to be(true)
         package.resources[0]["path"] = "foo.txt"
         expect( @validator.csv?( package.resources[0], package ) ).to be(false)
     end
-                           
+
     it "should detect fields missing from schema" do
         package = DataPackage::Package.new(test_package_filename)
         package.resources[0]["schema"]["fields"].delete_at(0)
         expect( @validator.valid?( package ) ).to be(false)
     end
-            
-    #it "should check encoding of CSV files is UTF-8" 
+
+    #it "should check encoding of CSV files is UTF-8"
 
 end
