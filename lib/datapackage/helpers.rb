8a0ac9cfd182ae02421509659c94e6206fad1a20
Georgiana Bere <bere.georgi@gmail.com>
2017-07-31 22:25:16 +0300

    Dereference Resource descriptors

--- lib/datapackage/helpers.rb
+++ lib/datapackage/helpers.rb
@@ -4,22 +4,25 @@ module DataPackage
     # Dereference a resource that can be a URL or path to a JSON file or a hash
     # Returns a Hash with all values that are URLs or paths dereferenced
     def dereference_descriptor(resource, base_path: nil, reference_fields: nil)
+      options = {
+        base_path: base_path,
+        reference_fields: reference_fields,
+      }
       case resource
       when Hash
         resource.inject({}) do |new_resource, (key, val)|
           if reference_fields.nil? || reference_fields.include?(key)
-            new_resource[key] = dereference_descriptor(val, base_path: base_path,
-              reference_fields: reference_fields)
+            new_resource[key] = dereference_descriptor(val, **options)
           else
             new_resource[key] = val
           end
           new_resource
         end
       when Enumerable
-        resource.map{ |el| dereference_descriptor(el, base_path: base_path, reference_fields: reference_fields)}
+        resource.map{ |el| dereference_descriptor(el, **options)}
       when String
         begin
-          resolve_json_reference(resource, base_path: base_path, deep_dereference: true)
+          resolve_json_reference(resource, deep_dereference: true, base_path: base_path)
         rescue Errno::ENOENT
           resource
         end
@@ -29,23 +32,22 @@ module DataPackage
     end
 
     # Resolve a reference to a JSON file; Returns the JSON as hash
-    # Raises JSON::ParserError, OpenURI::HTTPError, SocketError, TypeError for invalid references or JSON
+    # Raises JSON::ParserError, OpenURI::HTTPError, SocketError or TypeError for invalid references
     def resolve_json_reference(reference, deep_dereference: false, base_path: nil)
       # Try to extract JSON from file or webpage
       reference = join_paths(base_path, reference)
-      extracted_ref = load_json(reference)
+      resolved_reference = load_json(reference)
       if deep_dereference == true
-        dereference_descriptor(extracted_ref, base_path: base_path)
+        dereference_descriptor(resolved_reference, base_path: base_path)
       else
-        extracted_ref
+        resolved_reference
       end
     end
 
     # Load JSON from path or URL;
     # Raises: Errno::ENOENT, OpenURI::HTTPError, SocketError, JSON::ParserError
-    def load_json(schema_reference)
-      json = open(schema_reference).read
-      JSON.parse json
+    def load_json(reference)
+      JSON.parse open(reference).read
     end
 
     def base_path(path_or_url)
